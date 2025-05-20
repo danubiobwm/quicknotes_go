fefa8a0496ffade2bec4373683c0cfc652dec0e0
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-05-20 17:38:31 -0300

    file http

--- main.go
+++ main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	// Create a tcp address
-	addr, err := net.Listen("tcp", ":5000")
-	if err != nil {
-		panic(err)
-	}
-
-	defer addr.Close()
-	for {
-		con, err := addr.Accept()
-		fmt.Println("Conexão estabelecida")
-		if err != nil {
-			panic(err)
+	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+			return
 		}
-		go func(c net.Conn) {
-			data, _ := bufio.NewReader(con).ReadString('\n')
-			fmt.Println(data)
-			con.Write([]byte("Sua Msg foi recebinda com sucesso\n"))
-			con.Close()
-			fmt.Println("Conexão encerrada")
-		}(con)
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		message := string(body)
+		fmt.Println("Received message:", message)
+
+		if strings.Contains(message, "sair") {
+			fmt.Println("Client requested disconnect (HTTP context)")
+			fmt.Fprintf(w, "Server acknowledged 'sair'. Disconnecting from HTTP perspective.")
+			return
+		}
+
+		fmt.Fprintf(w, "Your message was received successfully: %s", message)
+	})
+
+	fmt.Println("HTTP server listening on :5000")
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		panic(err)
 	}
 }
