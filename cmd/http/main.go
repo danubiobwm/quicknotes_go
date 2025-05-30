bf677d14e0b913d05bb027610b0e29abad4fca40
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-05-30 19:59:46 -0300

    db

---
+++ cmd/http/main.go
@@ -0,0 +1,59 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	config := loadConfig()
+
+	slog.SetDefault(newLogger(os.Stderr, config.GetLevelLog()))
+
+	dbpool, err := pgxpool.New(context.Background(), config.DBConnURL)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
+	slog.Info("Conexão com o banco aconteceu com sucesso")
+
+	defer dbpool.Close()
+
+	slog.Info(fmt.Sprintf("Servidor rodando na porta %s\n", config.ServerPort))
+
+	mailPort, _ := strconv.Atoi(config.MailPort)
+	mailService := mailer.NewSMTPMailService(mailer.SMTPConfig{
+		Host:     config.MailHost,
+		Port:     mailPort,
+		Username: config.MailUsername,
+		Password: config.MailPassword,
+		From:     config.MailFrom,
+	})
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = time.Hour
+	sessionManager.Store = pgxstore.New(dbpool)
+	//Limpa as sessões expiradas da tabela de sessions a cada 30 minutos
+	pgxstore.NewWithCleanupInterval(dbpool, 30*time.Second)
+
+	csrfMiddleware := csrf.Protect([]byte(config.CSRFKey))
+
+	mux := LoadRoutes(sessionManager, mailService, dbpool)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", config.ServerPort), sessionManager.LoadAndSave(csrfMiddleware(mux))); err != nil {
+		panic(err)
+	}
+
+	// Configuração do TLS com certificados autoassinados
+	// if err := http.ListenAndServeTLS(fmt.Sprintf(":%s", config.ServerPort), "cer.cer", "cer.key", sessionManager.LoadAndSave(csrfMiddleware(mux))); err != nil {
+	// 	panic(err)
+	// }
+}
---
+++ cmd/http/routes.go
@@ -0,0 +1,64 @@
+package http
+
+import (
+	"io/fs"
+	"log/slog"
+	"net/http"
+
+	"github.com/danubiobwm/quicknotes_go/mailer"
+	"github.com/danubiobwm/quicknotes_go/views"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func LoadRoutes(sessionManager *scs.SessionManager, mail mailer.MailService, dbpool *pgxpool.Pool) http.Handler {
+	mux := http.NewServeMux()
+
+	static, err := fs.Sub(views.Files, "static")
+	if err != nil {
+		slog.Error(err.Error())
+		panic(err)
+	}
+
+	staticHandler := http.FileServerFS(static)
+
+	mux.Handle("GET /static/", http.StripPrefix("/static/", staticHandler))
+
+	noteRepo := repositories.NewNoteRepository(dbpool)
+	userRepo := repositories.NewUserRepository(dbpool)
+
+	render := render.NewRender(sessionManager)
+
+	noteHandler := handlers.NewNoteHandler(render, sessionManager, noteRepo)
+	userHandler := handlers.NewUserHandler(render, sessionManager, mail, userRepo)
+
+	authMidd := handlers.NewAuthMiddleware(sessionManager)
+	errorMidd := handlers.NewErrorHandlerMiddleware(render)
+
+	mux.HandleFunc("GET /", handlers.NewHomeHandler(render).HomeHandler)
+
+	mux.Handle("GET /note", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteList)))
+	mux.Handle("GET /note/{id}", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteView)))
+	mux.Handle("GET /note/new", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteNew)))
+	mux.Handle("POST /note", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteSave)))
+	mux.Handle("DELETE /note/{id}", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteDelete)))
+	mux.Handle("GET /note/{id}/edit", authMidd.RequireAuth(errorMidd.HandleError(noteHandler.NoteEdit)))
+
+	mux.Handle("GET /user/signup", errorMidd.HandleError(userHandler.SignupForm))
+	mux.Handle("POST /user/signup", errorMidd.HandleError(userHandler.Signup))
+
+	mux.Handle("GET /user/signin", errorMidd.HandleError(userHandler.SigninForm))
+	mux.Handle("POST /user/signin", errorMidd.HandleError(userHandler.Signin))
+
+	mux.Handle("GET /user/signout", errorMidd.HandleError(userHandler.Signout))
+
+	mux.Handle("GET /user/forgetpassword", errorMidd.HandleError(userHandler.ForgetPasswordForm))
+	mux.Handle("POST /user/forgetpassword", errorMidd.HandleError(userHandler.ForgetPassword))
+	mux.Handle("POST /user/password", errorMidd.HandleError(userHandler.ResetPassword))
+	mux.Handle("GET /user/password/{token}", errorMidd.HandleError(userHandler.ResetPasswordForm))
+
+	mux.Handle("GET /me", authMidd.RequireAuth(errorMidd.HandleError(userHandler.Me)))
+
+	mux.Handle("GET /confirmation/{token}", errorMidd.HandleError(userHandler.Confirm))
+
+	return mux
+}
